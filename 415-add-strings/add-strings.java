e2368d941bbfdd84a68666093ea67c5d90b255b7
Aryan Sadotra <aryansadotraparul@gmail.com>
2026-07-13 10:15:57 +0530

    Time: 2 ms (98.63%) | Memory: 43.7 MB (77.76%) - LeetSync

---
+++ 415-add-strings/add-strings.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+
+        StringBuilder sb = new StringBuilder();
+
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int r = 0;
+
+        while (i >= 0 || j >= 0 || r != 0) {
+
+            int d1 = 0;
+            int d2 = 0;
+
+            if (i >= 0) {
+                d1 = num1.charAt(i) - '0';
+                i--;
+            }
+
+            if (j >= 0) {
+                d2 = num2.charAt(j) - '0';
+                j--;
+            }
+
+            int sum = d1 + d2 + r;
+            int d = sum % 10;
+            r = sum / 10;
+
+            sb.append(d);
+        }
+
+        return sb.reverse().toString();
+    }
+}
