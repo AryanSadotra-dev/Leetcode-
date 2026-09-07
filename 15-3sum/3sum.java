a99488df0b9ff1b93c851337e5cddbf23d3dfb8d
Aryan Sadotra <aryansadotraparul@gmail.com>
2026-09-07 11:28:24 +0530

    Time: 36 ms (28.65%) | Memory: 59 MB (85.92%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,39 +1,49 @@
-import java.util.*;
 
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
-        int n = nums.length;
         Arrays.sort(nums);
-
-        Set<List<Integer>> ans = new HashSet<>();
-
+        List<List<Integer>> result = new ArrayList<>();
+        int n = nums.length;
         for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = n - 1;
 
-            int j = i + 1;
-            int k = n - 1;
-
-            while (j < k) {
-
-                int sum = nums[i] + nums[j] + nums[k];
-
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
                 if (sum == 0) {
 
-                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-                    k--;
+                    result.add(Arrays.asList(
+                        nums[i],
+                        nums[left],
+                        nums[right]
+                    ));
 
-                } else if (sum > 0) {
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
 
-                    k--;
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
 
-                } else {
+                    left++;
+                    right--;
 
-                    j++;
+                } 
+                else if (sum < 0) {
+                    left++;
+                } 
+                else {
+                    right--;
                 }
             }
         }
 
-        return new ArrayList<>(ans);
+        return result;
     }
 }
+
+
